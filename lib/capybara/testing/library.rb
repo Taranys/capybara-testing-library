c6cab1c27a0ba0296ff9ec35f4a0d8b8373290dc
Yoann Prot <yoann.prot@doctolib.com>
2021-10-25 14:50:00 +0200

    :tada: first version of the code

---
+++ lib/capybara/testing/library.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "library/version"
+
+module Capybara
+  module Testing
+    module Library
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capybara/testing/library/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Capybara
+  module Testing
+    module Library
+      VERSION = "0.1.0"
+    end
+  end
+end
