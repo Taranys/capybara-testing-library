7e372cc72950527816296a2280a2e3a5f35f1c6b
Yoann Prot <yoann.prot@doctolib.com>
2021-10-25 15:16:24 +0200

    :white_check_mark: remove falsy test

--- test/capybara/testing/library_test.rb
+++ test/capybara/testing/library_test.rb
@@ -6,8 +6,4 @@ class Capybara::Testing::LibraryTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Capybara::Testing::Library::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
