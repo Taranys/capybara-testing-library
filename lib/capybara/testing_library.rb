b57e5221f9bb3fffdf41393dc97b805452f5822d
Yoann Prot <yoann.prot@doctolib.com>
2022-10-07 09:19:18 +0200

    :rotating_light:

--- lib/capybara/testing_library.rb
+++ lib/capybara/testing_library.rb
@@ -9,14 +9,14 @@ module Capybara
   module TestingLibrary
     def assert_role_heading(text)
       query = XPath.or([
-         XPath.find_by_tagname('h1', text: text),
-         XPath.find_by_tagname('h2', text: text),
-         XPath.find_by_tagname('h3', text: text),
-         XPath.find_by_tagname('h4', text: text),
-         XPath.find_by_tagname('h5', text: text),
-         XPath.find_by_tagname('h6', text: text),
-         XPath.find_by_role_attribute('heading', text: text)
-       ])
+                         XPath.find_by_tagname("h1", text: text),
+                         XPath.find_by_tagname("h2", text: text),
+                         XPath.find_by_tagname("h3", text: text),
+                         XPath.find_by_tagname("h4", text: text),
+                         XPath.find_by_tagname("h5", text: text),
+                         XPath.find_by_tagname("h6", text: text),
+                         XPath.find_by_role_attribute("heading", text: text)
+                       ])
 
       assert_selector :xpath, query
     end
